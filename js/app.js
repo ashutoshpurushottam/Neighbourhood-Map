2b92c500c33782bd033ebd468decd72bb5fca24c
ashutoshpurushottam <apurushottam@icloud.com>
2016-11-04 15:01:56 +0530

    knockout integrated with google map API (showing map with no markers)

--- js/app.js
+++ js/app.js
@@ -1,15 +1,39 @@
 var map;
 
-function initMap() {
-	map = new google.maps.Map(document.getElementById('map-container'), {
-		center: {lat: 12.9716, lng: 77.5946},
-		zoom: 10
-    });
+var ViewModel = function() {
+	'use strict';
 
-    var bengaluru = {lat: 12.9716, lng: 77.5946};
-    var marker = new google.maps.Marker({
-    	position: bengaluru,
-    	map: map
-    });
+	var self = this;
+
+	var initialize = function() {
+		console.log("initMap called");
+		map = new google.maps.Map(document.getElementById('map-container'), {
+			center: {lat: 12.9716, lng: 77.5946},
+			zoom: 10
+    	});
+
+	}
+
+	google.maps.event.addDomListener(window, 'load', initialize);
 
 }
+
+ko.applyBindings(new ViewModel());
+
+
+
+// var map;
+
+// function initMap() {
+	// map = new google.maps.Map(document.getElementById('map-container'), {
+	// 	center: {lat: 12.9716, lng: 77.5946},
+	// 	zoom: 10
+ //    });
+
+ //    var bengaluru = {lat: 12.9716, lng: 77.5946};
+ //    var marker = new google.maps.Marker({
+ //    	position: bengaluru,
+ //    	map: map
+ //    });
+
+// }
